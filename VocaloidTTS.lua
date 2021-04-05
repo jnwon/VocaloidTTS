e3fe0b3e7fe4f70c2ac7ee6af43d8a0eb824f7b6
jnwon <atalanta16@naver.com>
2021-04-06 04:56:52 +0900

    first commit

---
+++ VocaloidTTS.lua
@@ -0,0 +1,17 @@
+function manifest()
+    myManifest = {
+        name          = "Vocaloid TTS",
+        comment       = "Text to Speech with Vocaloid",
+        author        = "spaghetti code",
+        pluginID      = "{4F2E5CF3-CAB2-46AE-862F-7C672CC50609}",
+        pluginVersion = "1.0.0.1",
+        apiVersion    = "3.0.0.1"
+    }
+
+    return myManifest
+end
+
+function main(processParam, envParam)
+
+end
+

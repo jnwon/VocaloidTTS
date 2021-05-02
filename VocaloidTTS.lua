7da7affecd2e7060c91fab241938f1ffb02ead71
jnwon <atalanta16@naver.com>
2021-05-03 04:47:41 +0900

    입력 dialog 테스트

--- VocaloidTTS.lua
+++ VocaloidTTS.lua
@@ -13,5 +13,30 @@ end
 
 function main(processParam, envParam)
 
+	-- パラメータ入力ダイアログのウィンドウタイトルを設定する.
+	VSDlgSetDialogTitle("Vocaloid TTS")
+
+	-- ダイアログにフィールドを追加する.
+	local field = {}
+	field.name       = "lyrics"
+	field.caption    = "Lyrics"
+	field.initialVal = ""
+	field.type       = 3
+	dlgStatus = VSDlgAddField(field)
+
+	-- ダイアログから入力値を取得する.
+	dlgStatus = VSDlgDoModal()
+	if (dlgStatus == 2) then
+		-- When it was cancelled.
+		return 0
+	end
+	if ((dlgStatus ~= 1) and (dlgStatus ~= 2)) then
+		-- When it returned an error.
+		return 1
+	end
+
+	-- ダイアログから入力値を取得する.
+	dlgStatus, lyricsInput = VSDlgGetStringValue("lyrics")
+	VSMessageBox(lyricsInput, 0)
 end
 

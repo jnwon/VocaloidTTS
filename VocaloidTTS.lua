99a0a19655ea459670a9c3c247ca0e6fd30b1843
jnwon <atalanta16@naver.com>
2021-05-05 11:11:03 +0900

    노트생성 테스트

--- VocaloidTTS.lua
+++ VocaloidTTS.lua
@@ -37,6 +37,32 @@ function main(processParam, envParam)
 
 	-- ダイアログから入力値を取得する.
 	dlgStatus, lyricsInput = VSDlgGetStringValue("lyrics")
-	VSMessageBox(lyricsInput, 0)
+	-- VSMessageBox(lyricsInput, 0)
+
+	-- VSSeekToBeginNote()
+	-- result, note = VSGetNextNote()
+	-- while result == 1 do
+	-- 	VSMessageBox(note.velocity, 0)
+	-- 	result, note = VSGetNextNote()
+	-- end
+
+	ebifry = {}
+	ebifry.posTick = {0, 120, 240, 360, 480}
+	ebifry.durTick = 120
+	ebifry.noteNum = {65, 67, 68, 65, 62}
+	ebifry.velocity = 64
+	ebifry.lyric = "エビフライ"
+	ebifry.phonemes = {"e", "b' i", "p\\ M", "4 a", "i"}
+
+	for i=1,5 do
+		note = {}
+		note.posTick = ebifry.posTick[i]
+		note.durTick = ebifry.durTick
+		note.noteNum = ebifry.noteNum[i]
+		note.velocity = ebifry.velocity
+		note.lyric = string.sub(ebifry.lyric, 3*i-2, 3*i)
+		note.phonemes = ebifry.phonemes[i]
+		VSInsertNote(note)
+	end
 end
 

4b5afadd5197dec06b4c3cf00998b20540b85de5
jnwon <atalanta16@naver.com>
2021-05-07 07:31:28 +0900

    papago 음성샘플 테스트

--- VocaloidTTS.lua
+++ VocaloidTTS.lua
@@ -11,6 +11,28 @@ function manifest()
     return myManifest
 end
 
+function SplitString(str, pat)
+   local t = {}
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+
+   while s do
+      if s ~= 1 or cap ~= "" then
+         table.insert(t,cap)
+      end
+     last_end = e+1
+     s, e, cap = str:find(fpat, last_end)
+   end
+
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+
+   return t
+end
+
 function main(processParam, envParam)
 
 	-- パラメータ入力ダイアログのウィンドウタイトルを設定する.
@@ -25,18 +47,18 @@ function main(processParam, envParam)
 	dlgStatus = VSDlgAddField(field)
 
 	-- ダイアログから入力値を取得する.
-	dlgStatus = VSDlgDoModal()
-	if (dlgStatus == 2) then
-		-- When it was cancelled.
-		return 0
-	end
-	if ((dlgStatus ~= 1) and (dlgStatus ~= 2)) then
-		-- When it returned an error.
-		return 1
-	end
+	-- dlgStatus = VSDlgDoModal()
+	-- if (dlgStatus == 2) then
+	-- 	-- When it was cancelled.
+	-- 	return 0
+	-- end
+	-- if ((dlgStatus ~= 1) and (dlgStatus ~= 2)) then
+	-- 	-- When it returned an error.
+	-- 	return 1
+	-- end
 
 	-- ダイアログから入力値を取得する.
-	dlgStatus, lyricsInput = VSDlgGetStringValue("lyrics")
+	-- dlgStatus, lyricsInput = VSDlgGetStringValue("lyrics")
 	-- VSMessageBox(lyricsInput, 0)
 
 	-- VSSeekToBeginNote()
@@ -54,15 +76,40 @@ function main(processParam, envParam)
 	ebifry.lyric = "エビフライ"
 	ebifry.phonemes = {"e", "b' i", "p\\ M", "4 a", "i"}
 
-	for i=1,5 do
-		note = {}
-		note.posTick = ebifry.posTick[i]
-		note.durTick = ebifry.durTick
-		note.noteNum = ebifry.noteNum[i]
-		note.velocity = ebifry.velocity
-		note.lyric = string.sub(ebifry.lyric, 3*i-2, 3*i)
-		note.phonemes = ebifry.phonemes[i]
-		VSInsertNote(note)
+	-- for i=1,5 do
+	-- 	note = {}
+	-- 	note.posTick = ebifry.posTick[i]
+	-- 	note.durTick = ebifry.durTick
+	-- 	note.noteNum = ebifry.noteNum[i]
+	-- 	note.velocity = ebifry.velocity
+	-- 	note.lyric = string.sub(ebifry.lyric, 3*i-2, 3*i)
+	-- 	note.phonemes = ebifry.phonemes[i]
+	-- 	VSInsertNote(note)
+	-- end
+
+	i = 0
+	for line in io.lines("notes(papago).txt") do
+		if (i > 0) then
+			tmpTable = SplitString(line, '%s')
+			if (math.floor(tonumber(tmpTable[1])) > 70) then
+				tmpTable[1] = 67
+			else
+				tmpTable[1] = math.floor(tonumber(tmpTable[1]))
+			end
+			note = {}
+			note.posTick = math.floor(tonumber(tmpTable[2]*1000))
+			note.durTick = math.floor(tonumber(tmpTable[3]*1000) - tonumber(tmpTable[2]*1000))
+			note.noteNum = tmpTable[1]
+			note.velocity = 64
+			note.lyric = tmpTable[4]
+			note.phonemes = "a"
+			VSInsertNote(note)
+			-- VSMessageBox(note.posTick..' '..note.durTick..' '..note.noteNum..' '..note.lyrics, 0)	
+			-- for k, v in pairs(tmpTable) do
+			-- 	VSMessageBox(v, 0)
+			-- end
+		end
+		i = i+1
 	end
 end
 
